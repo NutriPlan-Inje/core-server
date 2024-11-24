12ef8195b545c10801e00fc708e77945d45f7dc1
KyeongYeongDEV <yeong7098@naver.com>
2024-11-24 16:19:48 +0900

    FIX : nutrientsRatio Controller 기저 처리

--- nodejs/src/api/controllers/nutrientsRatio.controller.ts
+++ nodejs/src/api/controllers/nutrientsRatio.controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { Inject, Service } from "typedi";
-import { MacronutrientRatioForDayResponseDTO, MacronutrientRatioResponseDTO } from "../../dto/response/nutrientRatio";
+import { MacronutrientRatioForDayResponseDTO, MacronutrientRatioForWeekResponseDTO, MacronutrientRatioResponseDTO } from "../../dto/response/nutrientRatio";
 import { NutrientsRatioServie } from "../../services/nutrientsRatio.service";
 
 @Service()
@@ -13,6 +13,10 @@ export default class NutrientsRatioController {
         const u_id : number = parseInt(req.params.u_id);
         const date : string = req.params.date;
         const macronutrientRatioResponseDTO : MacronutrientRatioResponseDTO = await this.nutrientsRatioServie.calculateMacronutrientRatioForDay({ u_id, date });
+
+        if (macronutrientRatioResponseDTO.statusCode === 404) {
+            return res.status(404).json(macronutrientRatioResponseDTO);    
+        }
         return res.status(200).json(macronutrientRatioResponseDTO);
     }
 
@@ -20,13 +24,21 @@ export default class NutrientsRatioController {
         const u_id : number = parseInt(req.params.u_id);
         const date : string = req.params.date;
         const macronutrientRatioForDayResponseDTO : MacronutrientRatioForDayResponseDTO = await this.nutrientsRatioServie.evaluateMacronutrientIntakeForDay({ u_id, date });
+
+        if (macronutrientRatioForDayResponseDTO.statusCode === 404) {
+            return res.status(404).json(macronutrientRatioForDayResponseDTO);
+        }
         return res.status(200).json(macronutrientRatioForDayResponseDTO);
     }
 
     evaluateMacronutrientIntakeForWeek = async (req : Request, res : Response, next : NextFunction) => {
         const u_id : number = parseInt(req.params.u_id);
         const date : string = req.params.date;
-        const re = await this.nutrientsRatioServie.calculateMacronutrientRatioForWeek({ u_id, date });
-        return res.status(200).json(re);
+        const  macronutrientRatioForWeekResponseDTO : MacronutrientRatioForWeekResponseDTO= await this.nutrientsRatioServie.calculateMacronutrientRatioForWeek({ u_id, date });
+
+        if (macronutrientRatioForWeekResponseDTO.statusCode === 404){
+            return res.status(404).json(macronutrientRatioForWeekResponseDTO);
+        }
+        return res.status(200).json(macronutrientRatioForWeekResponseDTO);
     }
 }
