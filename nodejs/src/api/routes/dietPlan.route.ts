dd2ba0587ec6cdbd71a46be7b7399c092a6e7167
KyeongYeongDEV <yeong7098@naver.com>
2024-12-04 15:06:08 +0900

    ADD : 성능 개선을 위해 자주 쓰는 데이터인 foodInfo를 MySQL 로더 실행시 미리 캐시에 저장

--- nodejs/src/api/routes/dietPlan.route.ts
+++ nodejs/src/api/routes/dietPlan.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import Container from "typedi";
 import { DietPlanController } from "../controllers/dietPlan.controller";
-import { cacheMiddleware } from "../middlewares/lru.middleware";
+import { cacheMiddleware } from "../middlewares/cache.middleware";
 
 export default ({ app } : { app : Router}) => {
     const route = Router();
--- nodejs/src/loaders/mysql.loader.ts
+++ nodejs/src/loaders/mysql.loader.ts
@@ -1,29 +1,52 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
+import Redis from 'ioredis';
 
 dotenv.config();
 
 export default async () => {
-    try{
+    try {
         const pool = mysql.createPool({
-            host : process.env.DB_HOST,
-            user : process.env.DB_USER,
-            password : process.env.DB_PASSWORD,
-            port : process.env.DB_PORT as unknown as number,
-            database : process.env.DB_DATABASE,
-            connectionLimit : parseInt(process.env.DB_CONNECTION_LIMIT || '10', 10),
-            waitForConnections : true,
-            queueLimit : 0
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            port: process.env.DB_PORT as unknown as number,
+            database: process.env.DB_DATABASE,
+            connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT || '10', 10),
+            waitForConnections: true,
+            queueLimit: 0,
         });
 
-        //디비 연결 여부를 테스트로 
-        const connection = await pool.getConnection(); 
+        // 디비 연결 여부를 테스트로 확인
+        const connection = await pool.getConnection();
         console.log('MySQL 연결 성공!');
         connection.release();
 
+        // FoodInfo 캐시에 올리기
+        const redis = new Redis({
+            host: "redis",
+            port: 6379,
+        });
+
+        const [allFoodInfo, field] = await connection.query(`SELECT * FROM foodInfo`);
+        const foodInfoArray = allFoodInfo as Array<any>;
+
+        for (const foodInfo of foodInfoArray) {
+            const redisKey = `foodInfo:${foodInfo.id}`;
+            
+            const redisValue = {
+                statusCode: 200,
+                message: "조회에 성공했습니다",
+                data: foodInfo,
+            };
+
+            await redis.set(redisKey, JSON.stringify(redisValue), "EX", 3600); // TTL 1시간
+            console.log(`✅ Redis에 저장됨: ${redisKey}`);
+        }
+
         return pool;
     } catch (error) {
-        console.error('MySQL 연결 오류 : ',error); 
-        throw error; 
-    }   
-} 
+        console.error('MySQL 연결 오류 : ', error);
+        throw error;
+    }
+};
